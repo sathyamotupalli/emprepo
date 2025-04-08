691dd61052b75065616231c584160a5e3db901c1
sathya <venkatsathya152003@gmail.com>
2025-04-08 12:27:15 +0530

    added display

--- EMPJDBC/src/com/hexaware/client/Empclient.java
+++ EMPJDBC/src/com/hexaware/client/Empclient.java
@@ -1,5 +1,12 @@
 package com.hexaware.client;
 
 public class Empclient {
+	public static void main(String args[])
+	{
+		System.out.println("welcome");
+	}
+	public static void display() {
+		System.out.println("Display");
+	}
 
 }

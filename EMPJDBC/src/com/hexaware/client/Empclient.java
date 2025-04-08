83d8e4d4f6d536b98742aa5ddac880a210a97bc5
vineetha2823 <vineethaboyidi2003@gmail.com>
2025-04-08 12:39:25 +0530

    added view results

--- EMPJDBC/src/com/hexaware/client/Empclient.java
+++ EMPJDBC/src/com/hexaware/client/Empclient.java
@@ -8,11 +8,7 @@ public class Empclient {
 	public static void display() {
 		System.out.println("Display");
 	}
-
-	public static void main(String[] args) {
-		System.out.println("Welcome");
 		ViewResults();
-		}
 	public static void ViewResults() {
 		System.out.println("View Results");
 	}

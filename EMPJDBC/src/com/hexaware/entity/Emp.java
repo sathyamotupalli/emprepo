b7bbc196cf5e02a7d885fd170454c5cad53c5ab8
vineetha2823 <vineethaboyidi2003@gmail.com>
2025-04-08 13:14:09 +0530

    Added string

--- EMPJDBC/src/com/hexaware/entity/Emp.java
+++ EMPJDBC/src/com/hexaware/entity/Emp.java
@@ -5,6 +5,14 @@ public class Emp {
 	private int empId;
 	private String empName;
 	private double empSal;
+	
+	
+	public Emp(int empId, String empName, double empSal) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empSal = empSal;
+	}
 	public int getEmpId() {
 		return empId;
 	}

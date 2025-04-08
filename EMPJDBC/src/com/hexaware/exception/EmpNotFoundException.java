297fdf64a622bc9992ba251647949f9135d93e8d
vineetha2823 <vineethaboyidi2003@gmail.com>
2025-04-08 11:26:17 +0530

    Added Entity Class

--- EMPJDBC/src/com/hexaware/exception/EmpNotFoundException.java
+++ EMPJDBC/src/com/hexaware/exception/EmpNotFoundException.java
@@ -1,5 +1,28 @@
 package com.hexaware.exception;
 
-public class EmpNotFoundException {
+public class EmpNotFoundException extends Exception {
 
+	public EmpNotFoundException() {
+		super();
+	}
+
+	public EmpNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public EmpNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public EmpNotFoundException(String message) {
+		super(message);
+	}
+
+	public EmpNotFoundException(Throwable cause) {
+		super(cause);
+
+	}
+
+	
 }
